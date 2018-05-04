6fe49af165447ab9d83646e9d43bb2ce463c29fa
於琦 <Moeyfishyq@outlook.com>
2018-05-04 12:39:21 +0800

    Add files via upload

--- StereoCaliberate.py
+++ StereoCaliberate.py
@@ -3,96 +3,93 @@ import cv2
 import glob
 from matplotlib import pyplot as plt
 
+# termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
+# 6*7 represents the size of chess board,you can change according to your own chessboard.
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
-criteria2 = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.00001)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
+imgpoints_left = [] # 2d points in image plane.
+imgpoints_right = [] # 2d points in image plane.
 
-imgpoints_l = [] # 2d points in image plane.
-imgpoints_r = [] # 2d points in image plane.
+images1 = glob.glob('data/left/*.jpg')
+images2 = glob.glob('data/right/*.jpg')
 
-images_l = glob.glob('./data/left/*.jpg')
+for i in range(12):
+    # for fname in images:
+    img_left = cv2.imread(images1[i])
+    gray_left = cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY)
+    img_right = cv2.imread(images2[i])
+    gray_right = cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY)
 
-images_r = glob.glob('./data/right/*.jpg')
+    image_size = gray_left.shape[::-1]
+    # Find the chess board corners
+    ret_left, corners_left = cv2.findChessboardCorners(gray_left, (7, 6), None)
+    ret_right, corners_right = cv2.findChessboardCorners(gray_right, (7, 6), None)
 
+    # If found, add object points, image points (after refining them)
+    if ret_left and ret_right:
+        objpoints.append(objp)
 
-assert len(images_l) == len(images_r), 'There must be equal number of left and right photos.'
+        corners2_left = cv2.cornerSubPix(gray_left, corners_left, (11, 11), (-1, -1), criteria)
+        imgpoints_left.append(corners2_left)
+        corners2_right = cv2.cornerSubPix(gray_right, corners_right, (11, 11), (-1, -1), criteria)
+        imgpoints_right.append(corners2_right)
 
-for i in range(len(images_l)):
-	img_l = cv2.imread(images_l[i])
-	gray_l = cv2.cvtColor(img_l,cv2.COLOR_BGR2GRAY)
-	ret_l, corners_l = cv2.findChessboardCorners(gray_l, (7,6),None)
-	
-	img_r = cv2.imread(images_r[i])
-	gray_r = cv2.cvtColor(img_r,cv2.COLOR_BGR2GRAY)
-	ret_r, corners_r = cv2.findChessboardCorners(gray_r, (7,6),None)
+        # # Draw and display the corners
+        # img_left = cv2.drawChessboardCorners(img_left, (7, 6), corners2_left, ret_left)
+        # img_right = cv2.drawChessboardCorners(img_right, (7, 6), corners2_right, ret_right)
+        # cv2.imshow('left img', img_left)
+        # cv2.imshow('right img', img_right)
+        # cv2.waitKey(0)
 
-	if ret_l and ret_r:
-		objpoints.append(objp)
-
-		cv2.cornerSubPix(gray_l,corners_l,(11,11),(-1,-1),criteria2)
-		cv2.cornerSubPix(gray_r,corners_r,(11,11),(-1,-1),criteria2)
-
-		imgpoints_l.append(corners_l)
-		imgpoints_r.append(corners_l)
-
-imgsize = gray_r.shape[::-1]
-
-ret_l, cameraMatrix_l, dist_l, rvecs_l, tvecs_l = cv2.calibrateCamera(objpoints, imgpoints_l, imgsize,None,None)
-ret_r, cameraMatrix_r, dist_r, rvecs_r, tvecs_r = cv2.calibrateCamera(objpoints, imgpoints_r, imgsize,None,None)
-
-retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cv2.stereoCalibrate(objpoints, imgpoints_l, \
-	imgpoints_r, cameraMatrix_l, dist_l, cameraMatrix_r, dist_r, imgsize, cv2.CALIB_USE_INTRINSIC_GUESS, criteria = criteria2)
-
-print ("Rotation Matrix:", R, "\n\n")
-print ("Translation Vector:", T, "\n\n")
-print ("Essential Matrix:", E, "\n\n")
-print ("Fundamtel Matrix:", F, "\n\n")
+ret, mtx_left, dist_left, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints_left, gray_left.shape[::-1], None,None)
+ret, mtx_right, dist_right, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints_right, gray_right.shape[::-1], None,None)
 
 
-R1 = R2 = np.empty([3,3])
-P1 = P2 = np.empty([3,4])
+(rms_stereo, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F) = \
+cv2.stereoCalibrate(objpoints, imgpoints_left, imgpoints_right, mtx_left, dist_left, mtx_right, dist_right,  gray_left.shape[::-1], flags=cv2.CALIB_FIX_INTRINSIC);
 
-Q = np.empty([4,4])
-
-cv2.stereoRectify(cameraMatrix_l, dist_l, cameraMatrix_r, dist_r, imgsize, R, T, R1, R2, P1, P2, Q, 0, alpha = 1)
-
-#print (R,'\n\n', T, '\n\n', R1,'\n\n', R2,'\n\n', P1,'\n\n', P2,'\n\n', Q)
-
-#print (np.shape(map_l1), np.shape(map_l2))
-#print (np.shape(map_r1), np.shape(map_r2))
-
-print (imgsize)
+print("\n\ncamera matrix 1:\n",cameraMatrix1)
+print("\n\ncamera matrix 2:\n",cameraMatrix2)
+print("\n\nRotation Matrix:\n",R)
+print("\n\nTransltion:\n",T)
+print("\n\nEssential Matrix:\n",E)
+print("\n\nFundamental Matrix:\n",F)
+print("\n\nDistortion Coefficients:\n",distCoeffs1,distCoeffs2)
 
 
-for i in range(len(images_l)):
-	img_l = cv2.imread(images_l[i])
-	img_r = cv2.imread(images_r[i])
-	
-	h_l, w_l = img_l.shape[:2]
-	h_r, w_r = img_r.shape[:2]
+# (width,height)
+size = (640, 480)
+# size of your picture
+left = cv2.imread('data/left/left01.jpg',0)
+right = cv2.imread('data/right/right01.jpg',0)
 
-	newMatrix_l, roi_l=cv2.getOptimalNewCameraMatrix(cameraMatrix_l,dist_l,(w_l,h_l),1,(w_l,h_l))
-	newMatrix_R, roi_r=cv2.getOptimalNewCameraMatrix(cameraMatrix_r,dist_r,(w_r,h_r),1,(w_r,h_r))
+R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, size, R, T)
+mapL1, mapL2 = cv2.initUndistortRectifyMap(cameraMatrix1, distCoeffs1, R1, P1, size, cv2.CV_16SC2)
+mapR1, mapR2 = cv2.initUndistortRectifyMap(cameraMatrix2, distCoeffs2, R2, P2, size, cv2.CV_16SC2)
 
-	map_l1, map_l2 = cv2.initUndistortRectifyMap(cameraMatrix1, distCoeffs1, R1, newMatrix_l, imgsize, cv2.CV_32FC1, None, None)
-	map_r1, map_r2 = cv2.initUndistortRectifyMap(cameraMatrix2, distCoeffs2, R1, newMatrix_R, imgsize, cv2.CV_32FC1, None, None)
 
-	#print (img_l)
-	#cv2.imshow("img_l"+str(i),img_l)
-	rec_l = cv2.remap(img_l, map_l1, map_l2, 1)
-	x,y,w,h = roi_l
-	rec_l = rec_l[y:y+h, x:x+w]
-	cv2.imwrite('./results/lcalibresult'+str(i)+'.jpg',rec_l)
+left_img_remap = cv2.remap(left, mapL1, mapL2, cv2.INTER_LINEAR)
+right_img_remap = cv2.remap(right, mapR1, mapR2, cv2.INTER_LINEAR)
+cv2.imshow('rectified left',left_img_remap)
+cv2.imshow('rectified right',right_img_remap)
 
-	rec_r = cv2.remap(img_r, map_r1, map_r2, 1)
-	x,y,w,h = roi_r
-	rec_r = rec_r[y:y+h, x:x+w]
-	cv2.imwrite('./results/rcalibresult'+str(i)+'.jpg',rec_r)
+cv2.waitKey(0)
+x,y,w,h = roi1
+left_img_remap = left_img_remap[y:y+h, x:x+w]
+
+x,y,w,h = roi2
+right_img_remap = right_img_remap[y:y+h, x:x+w]
+
+print ("Valid Area:")
+cv2.imshow('rectified left',left_img_remap)
+cv2.imshow('rectified right',right_img_remap)
+
+cv2.waitKey(0)
+
 
